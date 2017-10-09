14ba57840125a2ab75554a3752c7aa5628405eb5
Lidia Gianne <lidiagianne@gmail.com>
2017-10-08 22:01:44 -0300

    Método GET e PUT implementados com HTTPS

--- MyDream_-_PHI.ino
+++ MyDream_-_PHI.ino
@@ -1,102 +1,83 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
- 
+#include <WiFiClientSecure.h>
+
 const char* ssid = "meuwifi";
 const char* password = "minhasenha";
-String host = "https://mydream-ufpa-phi.herokuapp.com/sensor";
-bool e_cel, e_tranca, e_vent, e_lamp1, e_lamp2, e_cortina, e_janela = false;
+const char* host = "mydream-ufpa-phi.herokuapp.com";
+bool e_cel, e_tranca, e_vent, e_lamp1, e_lamp2, e_cortina, e_janela;
 String senha = "1234";
+bool online = false;
 
 WiFiServer server(5000);
-WiFiClient client;
-
+WiFiClientSecure client;
 
 void conectar(){
   Serial.begin(115200);
-  delay(10);
- 
   Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
+  Serial.print("connecting to ");
   Serial.println(ssid);
- 
   WiFi.begin(ssid, password);
- 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
- 
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("connecting to ");
+  Serial.println(host);
   server.begin();
-  Serial.println("Server started");  
-
-  Serial.print("Use this URL : ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
-}
-
-void conexaoInicio(){
-  client = server.available();
-  if (!client) {
+  if (!client.connect(host, 443)) {
+    Serial.println("connection failed");
     return;
   }
- 
-  Serial.println("new client");
-  while(!client.available()){
-    delay(1);
-  }
-
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
 }
 
 String getRequest(String quem){
-  Serial.println("\nPegando estado\n");
-  if ( !client.connect("https://mydream-ufpa-phi.herokuapp.com/sensor", 5000) ) {
+  if ( !client.connect("mydream-ufpa-phi.herokuapp.com", 443) ) {
+    online = false;
     Serial.println("conexao falhou");
     return "connection failed";
   }
+
+  online = true;
   
-  client.println("GET " + quem + " HTTP/1.1");
-  client.print("Host: ");
-  client.println("https://mydream-ufpa-phi.herokuapp.com/sensor");
-  client.println("Connection: close");
-  client.println();
+  client.print(String("GET ") + quem + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "User-Agent: BuildFailureDetectorESP8266\r\n" +
+               "Connection: close\r\n\r\n");
  
   while (client.connected()) {
-    String data = client.readStringUntil('\n'); 
-    Serial.println(data);
-    if (data == "\r") {    
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
       break;
     }
   }
-  String data = client.readStringUntil('\n');
-  Serial.println("Dados recebidos\n");
-  Serial.println(data);  
-  return data;  
-}
-
-void conexaoFim(){
-  delay(1);
-  Serial.println("Client disconnected");
-  Serial.println("");
+  String line = client.readStringUntil('\n');
+  Serial.println(line);
+  return line.substring(1, line.length() - 1);  
 }
 
 void putRequest(String quem, String estado){
-  if ( !client.connect("https://mydream-ufpa-phi.herokuapp.com/sensor", 5000) ) {
+  if ( !client.connect("mydream-ufpa-phi.herokuapp.com", 443) ) {
     Serial.println("conexao falhou");
+    online = false;
+    return;
   }
-  
-  client.println("PUT " + quem + " HTTP/1.1");
-  client.print("Host: ");
-  client.println("https://mydream-ufpa-phi.herokuapp.com/sensor");
-  client.println("Connection: close");
-  client.println("{estado:" + estado + "}");
-  client.println();
+
+  online = true;
+
+  String data = "{\"estado\":\"" + estado + "\"}";
+
+  client.print(String("PUT ") + quem + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 //"Connection: close\r\n" +
+                 "Content-Type: application/json\r\n" +
+                 "Content-Length: " + data.length() + "\r\n" +
+                 "\r\n" + // This is the extra CR+LF pair to signify the start of a body
+                 data + "\n");
 }
 
 bool estados(String result){
@@ -116,24 +97,24 @@ String voltaEstados(bool result){
 }
 
 void atualizarEstados(){
-  e_cel = estados(getRequest("/celular"));
-  e_tranca = estados(getRequest("/e_tranca"));
-  e_vent = estados(getRequest("/e_vent"));
-  e_lamp1 = estados(getRequest("/e_lamp1"));
-  e_lamp2 = estados(getRequest("/e_lamp2"));
-  e_cortina = estados(getRequest("/e_cortina"));
-  e_janela = estados(getRequest("/e_janela"));
-  senha = getRequest("/senha");    
+  e_cel = estados(getRequest("/sensor/celular"));
+  e_tranca = estados(getRequest("/sensor/e_tranca"));
+  e_vent = estados(getRequest("/sensor/e_vent"));
+  e_lamp1 = estados(getRequest("/sensor/e_lamp1"));
+  e_lamp2 = estados(getRequest("/sensor/e_lamp2"));
+  e_cortina = estados(getRequest("/sensor/e_cortina"));
+  e_janela = estados(getRequest("/sensor/e_janela"));
+  senha = getRequest("/sensor/senha");    
 }
 
 void atualizarApi(){
-  putRequest("/celular", voltaEstados(e_cel));
-  putRequest("/e_tranca", voltaEstados(e_tranca));
-  putRequest("/e_vent", voltaEstados(e_vent));
-  putRequest("/e_lamp1", voltaEstados(e_lamp1));
-  putRequest("/e_lamp2", voltaEstados(e_lamp2));
-  putRequest("/e_cortina", voltaEstados(e_cortina));
-  putRequest("/e_janela", voltaEstados(e_janela));
+  putRequest("/sensor/celular", voltaEstados(e_cel));
+  putRequest("/sensor/e_tranca", voltaEstados(e_tranca));
+  putRequest("/sensor/e_vent", voltaEstados(e_vent));
+  putRequest("/sensor/e_lamp1", voltaEstados(e_lamp1));
+  putRequest("/sensor/e_lamp2", voltaEstados(e_lamp2));
+  putRequest("/sensor/e_cortina", voltaEstados(e_cortina));
+  putRequest("/sensor/e_janela", voltaEstados(e_janela));
 }
 
 void setup (){
@@ -141,15 +122,18 @@ void setup (){
 }
  
 void loop() { 
-  //interaçoes aki
-  //fazer flag pra put request a cada interaçao
-  
-  conexaoInicio();
-  
-  if(getRequest("/celular") == "desligado"){
+  if(getRequest("/sensor/celular") == "desligado"){
     atualizarApi();
   } else{
     atualizarEstados();    
   }
-  conexaoFim();
+  
+  if(!online){
+    //interaçoes aki
+    //fazer flag pra put request a cada interaçao  
+    
+  } else{
+    //checa todos os estados e faz os modulos obedecerem
+
+  }
 }
